543a59d94184b4c78753ca4175376554a0dcb3e7
renliqiang <1593685528@qq.com>
2018-02-02 16:02:36 +0800

    ceshi

---
+++ String3.java
@@ -0,0 +1,14 @@
+package zifuchuan;
+
+public class String3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="hello world";
+		System.out.println(str.replace(str, "H"));
+	}
+
+}
